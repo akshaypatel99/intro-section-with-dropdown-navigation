e3462752d2184b71bbb23522198e32348ee6331d
akshaypatel99 <akshaypatel99@gmail.com>
2023-01-04 15:50:12 +0000

    Update js for refactored html

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const menuBtn = document.getElementById('menuBtn');
-const subMenuLinks = document.querySelectorAll('.sub-menu');
+const primaryNavigation = document.getElementById('primary-navigation');
+const overlay = document.getElementById('overlay');
+const header = document.getElementById('primary-header');
+
+const subMenuBtns = document.querySelectorAll('.sub-menu-btn');
+const subMenus = document.querySelectorAll('.sub-menu');
 
 menuBtn.addEventListener('click', () => {
-	const menuIcon = document.getElementById('toggle-menu');
-	const overlay = document.getElementById('overlay');
-	const menu = document.getElementById('menu');
-	const header = document.body.children[0];
+	const visibility = primaryNavigation.getAttribute('data-visible');
 
-	const source = menuIcon.src.slice(-13, menuIcon.src.length);
-	if (source === 'icon-menu.svg') {
-		menuIcon.src = './images/icon-close-menu.svg';
-		overlay.style.visibility = 'visible';
-		overlay.style.right = 0;
-		menu.style.visibility = 'visible';
-		menu.style.right = 0;
-		header.style.overflow = 'visible';
+	if (visibility === 'false') {
+		primaryNavigation.setAttribute('data-visible', true);
+		header.setAttribute('data-visible', true);
+		overlay.setAttribute('data-visible', true);
+		menuBtn.setAttribute('aria-expanded', true);
 	} else {
-		menuIcon.src = './images/icon-menu.svg';
-		overlay.style.visibility = 'hidden';
-		overlay.style.right = '-100%';
-		menu.style.visibility = 'hidden';
-		menu.style.right = '-100%';
-		header.style.overflow = 'hidden';
+		primaryNavigation.setAttribute('data-visible', false);
+		header.setAttribute('data-visible', false);
+		overlay.setAttribute('data-visible', false);
+		menuBtn.setAttribute('aria-expanded', false);
 	}
 });
 
-subMenuLinks.forEach((link) => {
-	link.addEventListener('click', () => {
-		const arrow = link.children[0];
-		const arrowSource = arrow.src.slice(-8, arrow.length);
-		arrow.src =
-			arrowSource === 'down.svg'
-				? './images/icon-arrow-up.svg'
-				: './images/icon-arrow-down.svg';
-		link.children[1].classList.toggle('open-sub-menu');
+subMenuBtns.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		const id = btn.getAttribute('aria-controls');
+		const subMenu = document.getElementById(id);
+		const visibility = subMenu.getAttribute('data-visible');
+
+		if (visibility === 'false') {
+			subMenu.setAttribute('data-visible', true);
+			btn.setAttribute('aria-expanded', true);
+		} else {
+			subMenu.setAttribute('data-visible', false);
+			btn.setAttribute('aria-expanded', false);
+		}
 	});
 });
